2048cbe32e6b99e493d6cd6434ba046e3ed5d331
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-05-04 03:20:22 +0400

    Updates

---
+++ precache-manifest.54016e961cc0856a94479ba92f2d3000.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c636ddef656cf281f766b3c685c453",
+    "url": "/ecom-temp/index.html"
+  },
+  {
+    "revision": "bd95b0caa5029abc5f80",
+    "url": "/ecom-temp/static/css/2.67ab7c24.chunk.css"
+  },
+  {
+    "revision": "ff5ecf404491a9386c8c",
+    "url": "/ecom-temp/static/css/main.a3525325.chunk.css"
+  },
+  {
+    "revision": "bd95b0caa5029abc5f80",
+    "url": "/ecom-temp/static/js/2.fbddd5c9.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/ecom-temp/static/js/2.fbddd5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5ecf404491a9386c8c",
+    "url": "/ecom-temp/static/js/main.02514cc7.chunk.js"
+  },
+  {
+    "revision": "6d90a82548da958414b2",
+    "url": "/ecom-temp/static/js/runtime-main.2ac5084a.js"
+  },
+  {
+    "revision": "b4febffc27659c105c2a04b37606570e",
+    "url": "/ecom-temp/static/media/1hover.b4febffc.png"
+  },
+  {
+    "revision": "4fa08a506717c61f5feb9477f9f051c8",
+    "url": "/ecom-temp/static/media/2.4fa08a50.png"
+  },
+  {
+    "revision": "5337bc787e8c0d91e4d4c667deb60fca",
+    "url": "/ecom-temp/static/media/3.5337bc78.png"
+  },
+  {
+    "revision": "de2f793b7f3bacc4a61288156ec5c7ca",
+    "url": "/ecom-temp/static/media/4.de2f793b.png"
+  },
+  {
+    "revision": "4fe2e143854a4407288bde611faf7e52",
+    "url": "/ecom-temp/static/media/4hover.4fe2e143.png"
+  },
+  {
+    "revision": "dd23f9bce64dcb4b748cf9b7b421dc2a",
+    "url": "/ecom-temp/static/media/5.dd23f9bc.png"
+  },
+  {
+    "revision": "17921f78486785624ed020ea16ee4647",
+    "url": "/ecom-temp/static/media/5hover.17921f78.png"
+  },
+  {
+    "revision": "418cef63ce0d7f0c23d77a09868f83f0",
+    "url": "/ecom-temp/static/media/6.418cef63.png"
+  },
+  {
+    "revision": "3f79c70eb718a7c0e8d3d7d141b00aeb",
+    "url": "/ecom-temp/static/media/6hover.3f79c70e.png"
+  },
+  {
+    "revision": "cffe6abdaabf4e5a7dce94f7982a36de",
+    "url": "/ecom-temp/static/media/accer.cffe6abd.png"
+  },
+  {
+    "revision": "9775000cd7261923daf2bec7107f8ed8",
+    "url": "/ecom-temp/static/media/anker.9775000c.png"
+  },
+  {
+    "revision": "628d64d7f54e818b468d10a62c1ef52c",
+    "url": "/ecom-temp/static/media/apple.628d64d7.png"
+  },
+  {
+    "revision": "4e162555aac72014810652392bb873a8",
+    "url": "/ecom-temp/static/media/genius.4e162555.jpg"
+  },
+  {
+    "revision": "64e84e56fd93bd40240c39c9543b17d5",
+    "url": "/ecom-temp/static/media/hoco.64e84e56.png"
+  },
+  {
+    "revision": "2c98fffa0de305ab1eb1812bf3bb2ab2",
+    "url": "/ecom-temp/static/media/honor.2c98fffa.png"
+  },
+  {
+    "revision": "3c361ea4e2b3a1c63128ac0e64c9364a",
+    "url": "/ecom-temp/static/media/item1.3c361ea4.png"
+  },
+  {
+    "revision": "673304e9549c0eb2ea1e58e216a10578",
+    "url": "/ecom-temp/static/media/item1.673304e9.png"
+  },
+  {
+    "revision": "d0aa10816b72fab59716597767acc403",
+    "url": "/ecom-temp/static/media/item1.d0aa1081.png"
+  },
+  {
+    "revision": "f9f8ff61763bc2799dd9799a9e9342e7",
+    "url": "/ecom-temp/static/media/item1.f9f8ff61.png"
+  },
+  {
+    "revision": "24e5aee8b2448e871e9069b5d83cf0ec",
+    "url": "/ecom-temp/static/media/item2.24e5aee8.png"
+  },
+  {
+    "revision": "52d809650dfd4a58a450ca496e3a4ebe",
+    "url": "/ecom-temp/static/media/item2.52d80965.png"
+  },
+  {
+    "revision": "ecda4310061149b508fbde0be6e63df3",
+    "url": "/ecom-temp/static/media/item2.ecda4310.png"
+  },
+  {
+    "revision": "1442f98adf6d02ffada6ce1a44c286bf",
+    "url": "/ecom-temp/static/media/item3.1442f98a.png"
+  },
+  {
+    "revision": "48227b67c449f1cae6f11a453d4422ae",
+    "url": "/ecom-temp/static/media/item3.48227b67.png"
+  },
+  {
+    "revision": "4fe316723fbb0a7e27ce679e75c40726",
+    "url": "/ecom-temp/static/media/item3.4fe31672.png"
+  },
+  {
+    "revision": "5ae9d600a09c2e20b1b9fc2427d74234",
+    "url": "/ecom-temp/static/media/item4.5ae9d600.png"
+  },
+  {
+    "revision": "a51ab90c361fd4a38686d4355bc3d7c2",
+    "url": "/ecom-temp/static/media/item4.a51ab90c.png"
+  },
+  {
+    "revision": "be96629531490baccaae5c7dccbb900c",
+    "url": "/ecom-temp/static/media/item4.be966295.png"
+  },
+  {
+    "revision": "1ebffea79a47c9f1bbf8e5b0529955de",
+    "url": "/ecom-temp/static/media/item5.1ebffea7.png"
+  },
+  {
+    "revision": "a9eb545ef6f7beb84e718cd1763d37bb",
+    "url": "/ecom-temp/static/media/item5.a9eb545e.png"
+  },
+  {
+    "revision": "08dd602d63ab01adb30ccca494367656",
+    "url": "/ecom-temp/static/media/item6.08dd602d.png"
+  },
+  {
+    "revision": "0a9f71ea870c9f996df735adbfc0f8ff",
+    "url": "/ecom-temp/static/media/item6.0a9f71ea.png"
+  },
+  {
+    "revision": "ef0a557ccce5478192c7ea076167b402",
+    "url": "/ecom-temp/static/media/item7.ef0a557c.png"
+  },
+  {
+    "revision": "fa7a11698fb699dc468b14b53c796c0c",
+    "url": "/ecom-temp/static/media/item8.fa7a1169.png"
+  },
+  {
+    "revision": "13ebdfe6a86abe102a3ee5f39587fe00",
+    "url": "/ecom-temp/static/media/logo.13ebdfe6.png"
+  },
+  {
+    "revision": "778312951563f34f5a186b171bbc7207",
+    "url": "/ecom-temp/static/media/remax.77831295.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/ecom-temp/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/ecom-temp/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/ecom-temp/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "8a4ea76d49c0cb65310a6d41689dfe52",
+    "url": "/ecom-temp/static/media/search.8a4ea76d.png"
+  },
+  {
+    "revision": "acdc634ff7651b9805c9a06d229a2aab",
+    "url": "/ecom-temp/static/media/slider2.acdc634f.png"
+  }
+]);
